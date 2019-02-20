5714e838649ef5982fd344aa2ceb688fa946b6b5
Walker Ericsson <wericsson@una.edu>
2019-02-20 06:34:34 +0000

    Fixed* OOBabies exercise.

--- OOBabies.py
+++ OOBabies.py
@@ -13,36 +13,47 @@ class BabyNames :
   def add_url(self, target):
     try:
       text = urllib.urlopen(target)
-    except:
+    except IOError:
       print "Error opening ",
       print target
     else:
       if text.info().gettype() == 'text/html':
-        ols = re.findall(r"<ol>.*?</ol>", text.read())
+        pagetext = text.read()
+        ols = re.findall(r"<ol.*?>.*?</ol>", pagetext)
+        if len(ols) == 0:
+          print "Found no ordered lists at " + target
+          errf = open('dump.txt','w')
+          errf.write(pagetext)
+          errf.close
         for ol in ols:
           lis = re.findall(r"<li>.*?</li>", ol)
-          rank = 0
+          rank = 1
           while rank <= len(lis):
             # Using the built in for/in construct would be tempting, but we
             # still need a counter anyway.
-            match = re.search(r'<a.*?>(.*?)</a>', lis[rank])
+            try:
+              match = re.search(r'>\s*(\w+?)\s*<', lis[rank-1])
+              if match.group(1) in self.namerankdict:
+                self.namerankdict[match.group(1)] = min(self.namerankdict[match.group(1)],rank)
+              else:
+                self.namerankdict[match.group(1)] = rank
+            except:
+              print "Error extracting name from the following:",
+              print lis[rank-1]
             rank += 1
-            if match.group(1) in self.namerankdict:
-              self.namerankdict[match.group(1)] = min(self.namerankdict[match.group(1)],rank)
-            else:
-              self.namerankdict[match.group(1)] = rank
+      else:
+        print "Not HTML?"
       text.close()
   
   def text(self):
-    outtext = '\n\n' + str(self.year) + '\n\n'
+    outtext = str(self.year) + '\n\n'
     outlist = []
-    for name, rank in self.namerankdict:
+    for name, rank in self.namerankdict.items():
       outlist.append(name + " " + str(rank))
     for namerankstring in sorted(outlist):
       outtext += namerankstring
       outtext += '\n'
     return outtext
-        
 
 def main():
   args = sys.argv[1:]
@@ -57,6 +68,7 @@ def main():
     summary = True
     del args[0]
   
+  # dict matching years with corresponding BabyNames objects
   Years = {}
 
   try:
@@ -64,11 +76,12 @@ def main():
   except IOError:
     print "Error connecting to ",
     print args[0]
-    raise
+    raise IOError
     # I mean, this is still fatal.
   else:
     if text.info().gettype() == 'text/html':
       uls = re.findall(r"\d+\sto\s\d+</h2><ul>.*?</ul>", text.read())
+      print "len(uls) = " + str(len(uls))
       for ul in uls:
         lis = re.findall(r"<li>.*?</li>", ul)
         for li in lis:
@@ -80,18 +93,18 @@ def main():
             Years[Year] = BabyNames(Year)
             Years[Year].add_url(urlparse.urlparse(args[0])[0] + "://" + urlparse.urlparse(args[0])[1] + match.group(1))
     text.close()
-  isopen = False
   if summary:
     try:
       f = open('babysummary.txt', 'w')
     except:
       print "Error writing to babysummary.txt. Abort, Retry, and Fail, in any order you wish."
     else:
-      isopen = True
-  for Year in sorted(Years.keys()):
-    if isopen:
-      f.write(Years[Year].text())
-    else:
+      for Year in sorted(Years.keys()):
+        f.write(Years[Year].text())
+        f.write('\n\n')
+      f.close()
+  else:
+    for Year in sorted(Years.keys()):
       print Years[Year].text()
           
 if __name__ == '__main__':
